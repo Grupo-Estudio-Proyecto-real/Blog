cbe492f8aa956fabbe6ca8ee0abad6008a3100a9
Sebastian Asuncion Montero <desarrollo.espinacas1966@gmail.com>
2024-04-01 22:36:12 +0200

    Commit initial - 01/04/2024

---
+++ src/main/java/com/Blog/HitPosts/HitPostsApplication.java
@@ -0,0 +1,13 @@
+package com.Blog.HitPosts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HitPostsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HitPostsApplication.class, args);
+	}
+
+}
